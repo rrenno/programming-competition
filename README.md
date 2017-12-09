9d65d2f46164c742a49554dbd23861bfc3149fdc
rrenno <ryan.t.renno@gmail.com>
2017-12-09 12:04:54 -0800

    Update readme

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 Usage: `python p.py '[(8, 3, 0), (0, 3, 8)]'`
 
 ##Move the presents to the transport docks!
-Usage: `python r.rb '[1, 1, 3, 3, 6, 4, 7]' '[[1, 4], [6, 7]]'`
+Python Usage: `python p2.py '[1, 1, 3, 3, 6, 4, 7]' '[[1, 4], [6, 7]]'`
+Ruby Usage: `ruby r.rb ????`
--- r.rb
+++ r.rb
@@ -1,6 +1,5 @@
-import sys,ast
 f,j=lambda l:[i for s in l for i in s],lambda k:ast.literal_eval(sys.argv[k])
-p,d=j(1),j(2)
+p,d=[1, 1, 3, 3, 6, 4, 7],[[1, 4], [6, 7]]
 b=[[] for i in range(len(d))]
 g=f([[i for i,e in enumerate(d) if q in range(e[0],e[1]+1)] for q in p])
 for i,q1 in enumerate(p):
