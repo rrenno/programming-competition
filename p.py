dc46607164beae17c6024ae067dd317eac4c17a2
rrenno <ryan.t.renno@gmail.com>
2017-12-09 10:47:51 -0800

    Revert to min'd version

--- p.py
+++ p.py
@@ -1,30 +1,13 @@
 import sys,ast
-def draw_a_box(b,max_height):
-	l,w,h = 0, 1, 2
-	p,s,t,u = [], b[h]+1, b[h]+b[w]+2, b[w]+1
-
-	for i in range(max_height):
-		fs=(' ' if i>u else '_') * (i-b[h]-1)
-		bw='+' if i in (0,s,t) else ('|' if i in range(1,s) else '/')
-		bs='-' if i in (0,s,t) else ' '
-		ts=' ' * (b[w] - (u-i if i<u else 0) - (i-b[h]-1 if i > s else 0))
-		tss='_' * (u-i)
-		tsw=('|' if i>u else ('/' if i<u else '+')) if i<t and i>0 else ''
-		es='_' if i<u+1 else ' '
-		p.append(fs+bw+bs*b[l]+bw+ts+tsw+tss+es if i<b[h]+b[w]+3 else ' '*(4+b[l]+b[w]))
-
-	p.reverse()
-
-	return p
-
-l, w, h = 0, 1, 2
-boxes = ast.literal_eval(sys.argv[1])
-max_height = 3 + max([b[h] + b[w] for b in boxes])
-space_bbox = (max_height, 1)
-space_box_count = len(boxes) - 1
-space_box_line_count = boxes[0][w] + 2
-box_bbox = [(max_height, 3 + b[l] + b[w]) for b in boxes]
-
-
-for r in range(max_height):
-	print ''.join([p[r] for p in [draw_a_box(b, max_height) for b in boxes]])
+z=lambda x,y,z:x if y else z
+def d(b,h):
+	k,l,m,n,o='| +_/'
+	e,f,g=b
+	p,s,t,u=[],g+1,g+f+2,f+1
+	for i in range(h):
+		p.append(z(z(l,i>u,n)*(i-g-1)+z(m,i in (0,s,t),z(k,i in range(1,s),o))+z('-',i in (0,s,t),l)*e+z(m,i in (0,s,t),z(k,i in range(1,s),o))+l*(f-z(u-i,i<u,0)-z(i-g-1,i>s,0))+z(z(k,i>u,z(o,i<u,m)),0<i<t,'')+n*(u-i)+z(n,i<u+1,l),i<g+f+3,l*(4+e+f)))
+	return p[::-1]
+c=ast.literal_eval(sys.argv[1])
+h=3+max([b[1]+b[2] for b in c])
+for r in range(h):
+	print ''.join([p[r] for p in [d(b, h) for b in c]])
